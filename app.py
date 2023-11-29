a5568a2a3d13420fbf909562c4297b2576f796af
Wen-Hao Wu <vance.wenhaowu@gmail.com>
2023-11-29 16:25:02 +0100

    Add training function using subprocess

---
+++ app.py
@@ -0,0 +1,77 @@
+# Gradio App
+import subprocess
+
+import gradio as gr
+
+
+async def start_training(selected_model, data_file_path, epochs_count, batch_size):
+    if data_file_path is None:
+        return "Please upload a data file."
+    elif selected_model is None:
+        return "Please choose a pre-trained model"
+
+    with open("temp/training_status.txt", "w") as file:
+        file.write("Training is about to Start!")
+
+    subprocess.Popen(
+        [
+            "python",
+            "external_train_script.py",
+            selected_model,
+            data_file_path,
+            str(epochs_count),
+            str(batch_size),
+        ]
+    )
+
+    # counter = 0
+    # while True:
+    #     status = check_training_status()
+    #     if status == "Completed":
+    #         return "Training completed."
+    #     else:
+    #         # Alternate the message every second
+    #         if counter % 2 == 0:
+    #             message = "Training started.."
+    #         else:
+    #             message = "Training started..."
+    #         counter += 1
+    #         await asyncio.sleep(1)  # Check every second
+    #         with open('temp/training_status.txt', 'w') as file:
+    #             file.write(message)
+    #         with open('temp/training_status.txt', 'r') as file:
+    #             temp = file.read()
+    #         return temp
+    #         # return message
+
+    status = check_training_status()
+    if status == "Completed":
+        return "Training completed."
+    else:
+        return "Training in progress"
+
+
+def check_training_status():
+    try:
+        with open("temp/training_status.txt", "r") as file:
+            status = file.read()
+        return status
+    except FileNotFoundError:
+        return "Training Status Not Found"
+
+
+iface = gr.Interface(
+    fn=start_training,
+    inputs=[
+        gr.Dropdown(
+            ["yolov8n.pt", "yolov8s.pt", "yolov8m.pt", "yolov8l.pt", "yolov8x.pt"],
+            label="Select Model",
+        ),
+        gr.File(label="Data File Path (.yaml file)"),
+        gr.Slider(1, 500, 100, label="Epochs Count"),
+        gr.Slider(1, 128, 32, label="Batch Size"),
+    ],
+    outputs="text",
+)
+
+iface.launch(share=True)
---
+++ external_train_script.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+import shutil
+
+from ultralytics import YOLO
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+
+def train_yolo(selected_model, data_file_path, epochs_count, batch_size):
+    temp_folder = "temp"
+    os.makedirs(temp_folder, exist_ok=True)
+    data_temp_path = os.path.join(temp_folder, "data.yaml")
+    shutil.copy(data_file_path, data_temp_path)
+
+    model = YOLO(selected_model)
+    results = model.train(data=data_temp_path, epochs=epochs_count, batch=batch_size)
+    # At the end of your train_yolo function
+    with open("temp/training_status.txt", "w") as file:
+        file.write("Completed")
+
+    return results
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run YOLO Training")
+    parser.add_argument("selected_model", type=str, help="Selected YOLO model")
+    parser.add_argument("data_file_path", type=str, help="Path to data file")
+    parser.add_argument("epochs_count", type=int, help="Number of epochs")
+    parser.add_argument("batch_size", type=int, help="Batch size for training")
+
+    args = parser.parse_args()
+
+    train_yolo(
+        args.selected_model, args.data_file_path, args.epochs_count, args.batch_size
+    )
